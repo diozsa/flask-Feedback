1b9d5d16067e6069c96ca8782c0aef80e3e2f7fa
Dan Iozsa <diozsa@yahoo.com>
2022-08-21 15:31:50 -0700

    flask-hashing-loging Feedback app

---
+++ forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, NumberRange, Email
+
+class LoginForm(FlaskForm):
+    """Login Form"""
+
+    username = StringField("Username", validators=[InputRequired(), Length(min=1, max=20)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=5, max=30)])
+
+
+class RegisterForm(FlaskForm):
+    """Registration Form"""
+
+    username = StringField("Username", validators=[InputRequired(), Length(min=1, max=20)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=5, max=30)])
+    email = StringField("Email",validators=[InputRequired(), Email(), Length(max=50)])
+    first_name = StringField("First Name",validators=[InputRequired(), Length(max=30)])
+    last_name = StringField("Last Name",validators=[InputRequired(), Length(max=30)])
+
+
+class FeedbackForm(FlaskForm):
+    """Add feedback form."""
+
+    title = StringField("Title", validators=[InputRequired(), Length(max=100)])
+    content = StringField("Content", validators=[InputRequired()])
+
+
+class DeleteForm(FlaskForm):
+    """Delete form -- this form is intentionally blank."""
